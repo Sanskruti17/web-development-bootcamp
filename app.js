b33cde0adcf19116c7c628fe66dfda2f44ac7051
Sanskruti17 <sanskrutinashikkar704@gmail.com>
2023-02-16 17:04:42 +0530

    Stop the defalut behaviour added a logic
    here we extract name of the cat and put into unordered list. and make input clear. we stop the default behaviour of the form using event dot preventDefault.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const form = document.querySelector("#shelterForm");
+const input = document.querySelector("#catName");
+const List = document.querySelector("#list")
 form.addEventListener("submit",function(e){
     e.preventDefault();
-    console.log("SUBMITTED THE FORM");
+    const catName = input.value;
+    const newLI = document.createElement("li");
+    newLI.innerText = catName;
+    List.append(newLI);
+    input.value ="";
 })
