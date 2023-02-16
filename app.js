97321eb8c3e4580e535f529c527924af751767a2
Sanskruti17 <sanskrutinashikkar704@gmail.com>
2023-02-16 17:56:42 +0530

    created a sample tweet form

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const form = document.querySelector("#shelterForm");
-const input = document.querySelector("#catName");
-const List = document.querySelector("#list")
+
 form.addEventListener("submit",function(e){
     e.preventDefault();
-    const catName = input.value;
-    const newLI = document.createElement("li");
-    newLI.innerText = catName;
-    List.append(newLI);
-    input.value ="";
+    const userNameInput = document.querySelectorAll('input')[0];
+    const tweetInput = document.querySelectorAll('input')[1];
+    console.log(userNameInput.value, tweetInput.value);
 })
