b81ebbe9dc050b7664759391ee54da8d781ea26c
Sanskruti17 <sanskrutinashikkar704@gmail.com>
2023-02-17 18:09:12 +0530

    created a function for tweets add and reset the tweet field

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const tweetForm = document.querySelector("#tweetForm");
-
+const tweetsContainer = document.querySelector("#tweets")
 tweetForm.addEventListener("submit",function(e){
     e.preventDefault();
     // const userNameInput = document.querySelectorAll('input')[0];
     // const tweetInput = document.querySelectorAll('input')[1];
 
-    console.log(tweetForm.elements.userName.value);
-    console.log(tweetForm.elements.tweet.value);
+    const userNameInput =tweetForm.elements.userName;
+    const tweetInput =tweetForm.elements.tweet;
+    addTweet(userNameInput.value,tweetInput.value);
+    userNameInput.value ="";
+    tweetInput.value ="";
+    
 })
+
+const addTweet = (userName, tweet) =>{
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(userName);
+    newTweet.append(bTag);
+    newTweet.append(`- ${tweet}`);
+    console.log(newTweet);
+    tweetsContainer.append(newTweet);
+
+}
